90108801a3fa4ddd23c003da8b8b1406e569c705
Sander <sanderspost.cybercity@gmail.com>
2021-10-29 16:43:06 +0200

    asdf

---
+++ DNPAssignment2/Data/IFamilyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace Blazor_Authentication.Data
+{
+    public interface IFamilyManager
+    {
+        void AddFamily(Family family);
+        void RemoveFamily(Family family);
+        IList<Family> GetFamilies();
+        void Update();
+        Family GetFamily(int familyId);
+
+        
+        
+        void RemoveAdult(int adultId);
+        IList<Adult> GetAdults();
+        Adult GetAdult(int id);
+        
+    }
+}
---
+++ DNPAssignment2/Data/Impl/FamilyManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FileData;
+using Models;
+
+namespace Blazor_Authentication.Data.Impl
+{
+    public class FamilyManager : IFamilyManager
+    {
+        private FileContext _fileContext;
+
+        
+        
+        public FamilyManager()
+        {
+            _fileContext = new FileContext();
+            Console.WriteLine("commentar");
+        }
+        public void AddFamily(Family family)
+        {
+            _fileContext.Families.Add(family);
+            _fileContext.SaveChanges();
+        }
+
+        public void RemoveFamily(Family family)
+        {
+            _fileContext.Families.Remove(family);
+            _fileContext.SaveChanges();
+        }
+
+        public IList<Family> GetFamilies()
+        {
+            return _fileContext.Families;
+        }
+
+        public void Update()
+        {
+            _fileContext.SaveChanges();
+        }
+        
+        public Family GetFamily(int familyId)
+        {
+            return _fileContext.Families.FirstOrDefault(family =>
+                family.FamilyId == familyId);
+        }
+
+        public void RemoveAdult(int adultId)
+        {
+            foreach (Family family in _fileContext.Families)
+            {
+                Adult adult = family.Adults.FirstOrDefault(adult =>
+                    adult.Id == adultId);
+
+                if (adult != null)
+                {
+                    family.Adults.Remove(adult);
+                    break;
+                }
+            }
+            
+            _fileContext.SaveChanges();
+        }
+
+        public IList<Adult> GetAdults()
+        {
+            List<Adult> _adults = new();
+
+            foreach (var family in _fileContext.Families)
+            {
+                _adults.AddRange(family.Adults);
+            }
+
+            return _adults;
+        }
+
+        public Adult GetAdult(int id)
+        {
+            return GetAdults().FirstOrDefault(adult =>
+                adult.Id == id);
+        }
+    }
+}
